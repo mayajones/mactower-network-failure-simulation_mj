0b72e08035d72e0ee876a6cdec3603e5bf9c4949
hongqin <hqinlab@gmail.com>
2014-04-10 08:49:30 -0400

    ms02 run finished
    ms02 run

--- ms02GINPPI/net-aging-sim-2014Feb24.R
+++ ms02GINPPI/net-aging-sim-2014Feb24.R
@@ -20,7 +20,7 @@ essenTb = read.csv('SummaryRegressionHetHom2013Oct29.csv', colClasses=rep('chara
 # remove self-intxns
 
 #for ( i in 1:100) #
-for( i in 3:100 ){ ##start at 3 after power outage, 20140407
+for( i in 1:100 ){ #20140410
   path = paste('dipgin.ms02.output/', i, sep='')
   ms02file = paste('ms02_', i, ".tab", sep='')
   infile = paste( path, '/', ms02file, sep=""); print(infile)
