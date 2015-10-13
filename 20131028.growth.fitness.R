1f1cc95919bf168baf471656d5d453090d831573
HongQin <hqinlab@gmail.com>
2015-10-12 23:49:14 -0400

    fix a bug in off parsing from growth fitness data
    a comma in orf cause parsing problem in read.csv(). Fixed by using
    quotes.

--- 20131028.growth.fitness.R
+++ 20131028.growth.fitness.R
@@ -1,5 +1,9 @@
+# update 20151012, try to fix a bug that resulted 1 pair of TRUE in output orf
+# This bug is caused by write.csv(, quote=F, row.names=F) 
+# The problem is caused by "DUR1,2" an ORF that contains a comma. 
+
 rm(list=ls())
-setwd("~/projects/0.network.aging.prj/0.ppi.reliability.simulation")
+setwd("~/github/mactower-network-failure-simulation")
 list.files(path="data/", pattern="Regression")
 my.files = c("Regression_Tc1_het.txt", "Regression_Tc1_hom.txt", "Regression_Tc2_het.txt", "Regression_Tc2_hom.txt")
 # I removed the single quotes in these files
@@ -13,22 +17,26 @@ tb1$AnnotationFlag = ifelse( is.na(tb1$YPD), 'questionable1', 'verified1' )
 table(tb1$AnnotationFlag)
 # questional verified 
 # 174         5744 
+x = table(tb1$orf); str(x)
 
 tb3$AnnotationFlag = ifelse( is.na(tb3$YPD), 'questionable2', 'verified2' )
 table(tb3$AnnotationFlag)
 # quetionable    verified 
 # 152        5766 
 hist(tb3$YPD)
+x = table(tb3$orf); str(x)
 
 tb2$GrowthFlag = ifelse( is.na(tb2$YPD), 'nogrowth1', 'growth1' )
 table(tb2$GrowthFlag)
 # growth nogrowth 
 # 4659     1259 
+x = table(tb2$orf); str(x)
 
 tb4$GrowthFlag = ifelse( is.na(tb4$YPD), 'nogrowth2', 'growth2' )
 table(tb4$GrowthFlag)
 # growth nogrowth 
 # 4718     1200 
+x = table(tb4$orf); str(x)
 
 tb12 = merge(tb1,tb2, by.x='orf', by.y='orf')
 tb12 = tb12[, c(1,2,grep('Flag',names(tb12)))]
@@ -63,6 +71,7 @@ table(tbReal$Growth1, tbReal$Growth2)
 tbReal[tbReal$Growth1=='nogrowth1' & tbReal$Growth2=='nogrowth2',  ] [1:10,]
 tbReal[tbReal$Growth1=='growth1' & tbReal$Growth2=='nogrowth2',  ] 
 
+#what was I doing here? 
 tbReal$essenflag = ifelse( tbReal$Growth1=='growth1' & tbReal$Growth2=='growth2', 'nonessential', 
                        ifelse(tbReal$Growth1=='nogrowth1' & tbReal$Growth2=='nogrowth2', 'essential', 'abnormal') )
 head(tbReal)
@@ -70,6 +79,10 @@ table(tbReal$essenflag)
 # abnormal    essential nonessential 
 # 68         1152         4552 
 #Good, consistent results. Use these for further analysis. 2013 Oct 29
+# 20151012, found two "TRUE" in orfs
+length(tbReal$orf) #5772
+length(unique(tbReal$orf)) #5772
 
-write.csv(tbReal, "SummaryRegressionHetHom2013Oct29.csv",  quote=F, row.names=F )
+write.csv(tbReal, "SummaryRegressionHetHom2015Oct12.csv" ) #change 20151012
+#write.csv(tbReal, "SummaryRegressionHetHom2015Oct12.csv",  quote=F, row.names=F )
 
